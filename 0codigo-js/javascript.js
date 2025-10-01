38eb0f28418ceb0cf199441e45656461a308f59e
Filipe Fernandes de Oliveira <filipe.fernandes2204@gmail.com>
2025-10-01 18:25:47 -0300

    fix: Caminho JS final corrigido com '../' relativo ao arquivo script

--- 0codigo-js/javascript.js
+++ 0codigo-js/javascript.js
@@ -1,9 +1,9 @@
 /* Lista de caminhos das minhas fotos. */
 const fotosTopo = [
-    '0imagens/photo-output.jpeg',
-    '0imagens/foto-filipe-espelho.png',
-    '0imagens/foto-filipe-live.png',
-    '0imagens/foto-filipe-selfie.png'
+    '../0imagens/photo-output.jpeg',
+    '../0imagens/foto-filipe-espelho.png',
+    '../0imagens/foto-filipe-live.png',
+    '../0imagens/foto-filipe-selfie.png'
 ];
 
 /* Contador que rastreia qual foto está sendo exibida (começa na posição 0). */
